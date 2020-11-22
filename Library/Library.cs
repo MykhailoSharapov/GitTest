98d746fd3dce55b39377b0e51691cdec92009dc3
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-22 14:22:28 +0200

    add input functions into library

--- Library/Library.cs
+++ Library/Library.cs
@@ -8,6 +8,33 @@ namespace Library
 {
     public class Library
     {
-
+        public bool GetIntegerInputParamets(out int value)
+        {
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                value = result;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Input parametr must be Integer");
+                value = result;
+                return false;
+            }
+        }
+        public bool GetDoubleInputParamets(out double value)
+        {
+            if (double.TryParse(Console.ReadLine(), out double result))
+            {
+                value = result;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Input parametr must be Double");
+                value = result;
+                return false;
+            }
+        }
     }
 }

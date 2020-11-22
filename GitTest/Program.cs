207056bd1dbc3f970994bbd39c309a5e10f8f1c0
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-22 14:29:41 +0200

    input parametrs validation

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -4,14 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Library;
+
 namespace GitTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool flag = false;
             Console.WriteLine("Please input parametr A:");
-            
+            int parametrA = 0;
+            do
+            {
+                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                parametrA = result;
+            }
+            while (!flag);
+            Console.WriteLine("Please input parametr B:");
+            int parametrB = 0;
+            do
+            {
+                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                parametrB = result;
+            }
+            while (!flag);
+            Console.WriteLine("Please input parametr C:");
+            int parametrC = 0;
+            do
+            {
+                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                parametrC = result;
+            }
+            while (!flag);
         }
     }
 }
--- Library/Library.cs
+++ Library/Library.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    public class Library
+    public class ConsoleManagment
     {
         public bool GetIntegerInputParamets(out int value)
         {

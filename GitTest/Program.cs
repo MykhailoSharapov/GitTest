127f9c82c013579b6acfcfd0d713a41de202f139
Mykhailo Sharapov <mikesharapov1@gmail.com>
2020-11-22 19:43:00 +0200

    Final version all done

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -12,51 +12,63 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            bool flag = false;
-            Console.WriteLine("Please input parametr A:");
-            int parametrA = 0;
+            bool isEndOfProg = false;
             do
             {
-                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
-                parametrA = result;
-                if(parametrA == 0)
-            {
-                    Console.WriteLine("ParamtrA is equal 0, korney ne budet , try again");
-                    flag = false;
+                bool flag = false;
+                
+                Console.WriteLine("Please input parametr A:");
+                int parametrA = 0;
+                do
+                {
+                    flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                    parametrA = result;
+                    if (parametrA == 0)
+                    {
+                        Console.WriteLine("ParamtrA is equal 0, korney ne budet , try again");
+                        flag = false;
+                    }
                 }
+                while (!flag);
+
+                Console.WriteLine("Please input parametr B:");
+                int parametrB = 0;
+                do
+                {
+                    flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                    parametrB = result;
+                }
+                while (!flag);
+                Console.WriteLine("Please input parametr C:");
+                int parametrC = 0;
+                do
+                {
+                    flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
+                    parametrC = result;
+                }
+                while (!flag);
+                float parametrD = (parametrB * parametrB) - (4 * parametrA * parametrC);
+                if (parametrD < 0)
+                {
+                    Console.WriteLine("korney net");
+                }
+                else if (parametrD == 0)
+                {
+                    Console.WriteLine("Your koren is {0}", new MathLib().GetKorenOdin(parametrB, parametrA, parametrD));
+                }
+                else
+                {
+                    Console.WriteLine("Your koren is {0}, and {1}", new MathLib().GetKorenOdin(parametrB, parametrA, parametrD), new MathLib().GetKorenDva(parametrB, parametrA, parametrD));
+                }
+                Console.WriteLine("Do you want to continue? y/n");
+                char isContinueChar = Console.ReadKey().KeyChar;
+                if(!isContinueChar.Equals('y'))
+                {
+                    isEndOfProg = true;
+                }
+                Console.Clear();
             }
-            while (!flag);
-                    
-            Console.WriteLine("Please input parametr B:");
-            int parametrB = 0;
-            do
-            {
-                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
-                parametrB = result;
-            }
-            while (!flag);
-            Console.WriteLine("Please input parametr C:");
-            int parametrC = 0;
-            do
-            {
-                flag = new ConsoleManagment().GetIntegerInputParamets(out int result);
-                parametrC = result;
-            }
-            while (!flag);
-            float parametrD = (parametrB * parametrB) - (4 * parametrA * parametrC);
-            if(parametrD < 0)
-            {
-                Console.WriteLine("korney net");
-            }
-            else if(parametrD == 0)
-            {
-                Console.WriteLine("Your koren is {0}", new MathLib().GetKorenOdin(parametrB,parametrA,parametrD));
-            }
-            else
-            {
-                Console.WriteLine("Your koren is {0}, and {1}", new MathLib().GetKorenOdin(parametrB, parametrA, parametrD), new MathLib().GetKorenDva(parametrB, parametrA, parametrD));
-            }
-            Console.ReadLine();
+            while (!isEndOfProg);
         }
     }
 }
